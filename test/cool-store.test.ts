6275bccce4cea2c86a72ca48bfe4210e027ae80a
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-02-05 10:52:58 +0700

    Test CoolStore

---
+++ test/cool-store.test.ts
@@ -0,0 +1,115 @@
+import { CoolStore } from '../src';
+import { take } from 'rxjs/operators';
+import clone from 'lodash.clonedeep';
+
+describe('CoolStore', () => {
+  interface State {
+    product: {
+      name: string;
+    };
+  }
+
+  let store: CoolStore<State>;
+
+  it('Create Store', () => {
+    const initialState: State = {
+      product: {
+        name: '',
+      },
+    };
+
+    store = new CoolStore<State>(initialState);
+
+    // Joker
+    initialState.product.name = 'iPhone';
+
+    expect(store.get()).toEqual({
+      product: {
+        name: '',
+      },
+    });
+  });
+
+  it('Set Entire State', () => {
+    const product = {
+      name: 'Galaxy',
+    };
+
+    store.set(() => ({ product }));
+
+    // Joker
+    product.name = 'iPhone';
+
+    expect(store.get()).toEqual({
+      product: {
+        name: 'Galaxy',
+      },
+    });
+  });
+
+  it('Set Chilren State', () => {
+    const product = {
+      name: 'iPhone',
+    };
+
+    store.set(state => {
+      state.product = product;
+    });
+
+    // Joker
+    product.name = 'Galaxy';
+
+    expect(store.get()).toEqual({
+      product: {
+        name: 'iPhone',
+      },
+    });
+  });
+
+  it('Get State', () => {
+    const state = store.get();
+
+    // Joker
+    state.product['name'] = 'Galaxy';
+
+    expect(store.get()).toEqual({
+      product: {
+        name: 'iPhone',
+      },
+    });
+  });
+
+  it('Get State Changes', done => {
+    store
+      .getChanges()
+      .pipe(take(2))
+      .subscribe({
+        next: state => {
+          const cloneState = clone(state);
+
+          // Joker
+          state.product['name'] = 'Galaxy';
+
+          expect(cloneState).toEqual(store.get());
+
+          // Set State
+          store.set(state => {
+            state.product.name = 'Galaxy';
+          });
+
+          expect(cloneState).toEqual(store.get());
+        },
+        complete: () => done(),
+      });
+  });
+
+  it('Reset State', () => {
+    store.reset();
+
+    expect(store.get()).toEqual({
+      product: {
+        name: '',
+      },
+    });
+  });
+});
