c01b656ac1d50ad15239f95b18f59260016cb369
Maxvien <maxvien@Maxviens-MacBook-Air.local>
2020-02-06 15:32:58 +0700

    Fix setData()

--- test/async-cool-store.test.ts
+++ test/async-cool-store.test.ts
@@ -30,24 +30,22 @@ describe('AsyncCoolStore', () => {
   });
 
   it('Set Entire Data 1', () => {
+    store.setData(() => null);
+
+    expect(store.get()).toEqual({
+      loading: false,
+      data: null,
+      error: null,
+    });
+  });
+
+  it('Set Entire Data 2', () => {
     store.setData({ name: 'Nokia' });
 
     expect(store.get()).toEqual({
       loading: false,
       data: {
         name: 'Nokia',
-      },
-      error: null,
-    });
-  });
-
-  it('Set Entire Data 2', () => {
-    store.setData(() => ({ name: 'iPhone' }));
-
-    expect(store.get()).toEqual({
-      loading: false,
-      data: {
-        name: 'iPhone',
       },
       error: null,
     });
